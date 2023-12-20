01edacca09ecc0fc9f2918776c8e3529f669f94d
Reid Wahl <nrwahl@protonmail.com>
2023-12-20 00:25:02 -0800

    Refactor: libcrmcommon: New PCMK__OPT_START_FAILURE_IS_FATAL constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options_internal.h
+++ include/crm/common/options_internal.h
@@ -160,6 +160,7 @@ bool pcmk__valid_sbd_timeout(const char *value);
 #define PCMK__OPT_SHUTDOWN_ESCALATION       "shutdown-escalation"
 #define PCMK__OPT_SHUTDOWN_LOCK             "shutdown-lock"
 #define PCMK__OPT_SHUTDOWN_LOCK_LIMIT       "shutdown-lock-limit"
+#define PCMK__OPT_START_FAILURE_IS_FATAL    "start-failure-is-fatal"
 
 // Constants for meta-attribute names
 #define PCMK__META_ALLOW_MIGRATE            "allow-migrate"
