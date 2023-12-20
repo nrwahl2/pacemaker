a5cce5c06db945b359e215cace80df13424c3abf
Reid Wahl <nrwahl@protonmail.com>
2023-12-20 00:48:30 -0800

    Refactor: libcrmcommon: New PCMK__OPT_STONITH_WATCHDOG_TIMEOUT constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options_internal.h
+++ include/crm/common/options_internal.h
@@ -166,6 +166,7 @@ bool pcmk__valid_sbd_timeout(const char *value);
 #define PCMK__OPT_STONITH_ENABLED           "stonith-enabled"
 #define PCMK__OPT_STONITH_MAX_ATTEMPTS      "stonith-max-attempts"
 #define PCMK__OPT_STONITH_TIMEOUT           "stonith-timeout"
+#define PCMK__OPT_STONITH_WATCHDOG_TIMEOUT  "stonith-watchdog-timeout"
 
 // Constants for meta-attribute names
 #define PCMK__META_ALLOW_MIGRATE            "allow-migrate"
