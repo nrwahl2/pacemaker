de52c9f45f84ce3c52cf106664d12ae9686bbcfa
Reid Wahl <nrwahl@protonmail.com>
2023-12-28 18:01:43 -0800

    Refactor: libcrmcommon: New PCMK__META_PROMOTED_MAX string constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/pengine/clone.c
+++ lib/pengine/clone.c
@@ -338,7 +338,7 @@ clone_unpack(pcmk_resource_t *rsc, pcmk_scheduler_t *scheduler)
     if (pcmk_is_set(rsc->flags, pcmk_rsc_promotable)) {
         // Use 1 as default but 0 for minimum and invalid
         // @COMPAT PCMK_XA_PROMOTED_MAX_LEGACY deprecated since 2.0.0
-        clone_data->promoted_max = unpack_meta_int(rsc, PCMK_META_PROMOTED_MAX,
+        clone_data->promoted_max = unpack_meta_int(rsc, PCMK__META_PROMOTED_MAX,
                                                    PCMK_XA_PROMOTED_MAX_LEGACY,
                                                    1);
 
