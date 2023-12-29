9b1f94c3207afcae0b4db88af0782d923b94c992
Reid Wahl <nrwahl@protonmail.com>
2023-12-28 20:32:49 -0800

    Refactor: libcrmcommon: New PCMK__META_IS_MANAGED string constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/pengine/complex.c
+++ lib/pengine/complex.c
@@ -747,7 +747,7 @@ pe__unpack_resource(xmlNode *xml_obj, pcmk_resource_t **rsc,
         pe__set_resource_flags(*rsc, pcmk_rsc_migratable);
     }
 
-    value = g_hash_table_lookup((*rsc)->meta, XML_RSC_ATTR_MANAGED);
+    value = g_hash_table_lookup((*rsc)->meta, PCMK__META_IS_MANAGED);
     if (value != NULL) {
         if (crm_is_true(value)) {
             pe__set_resource_flags(*rsc, pcmk_rsc_managed);
