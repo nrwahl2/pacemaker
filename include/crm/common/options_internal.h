d0e7420597519abb9cacae9e66c21a5d1ff34b39
Reid Wahl <nrwahl@protonmail.com>
2023-12-28 16:27:44 -0800

    Refactor: libcrmcommon: New PCMK__META_INTERLEAVE string constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options_internal.h
+++ include/crm/common/options_internal.h
@@ -286,6 +286,7 @@ bool pcmk__valid_sbd_timeout(const char *value);
 #define PCMK__META_CONTAINER_ATTR_TARGET    "container-attribute-target"
 #define PCMK__META_ENABLED                  "enabled"
 #define PCMK__META_FAILURE_TIMEOUT          "failure-timeout"
+#define PCMK__META_INTERLEAVE               "interleave"
 #define PCMK__META_MIGRATION_THRESHOLD      "migration-threshold"
 #define PCMK__META_ORDERED                  "ordered"
 #define PCMK__META_PROMOTED_MAX             "promoted-max"
