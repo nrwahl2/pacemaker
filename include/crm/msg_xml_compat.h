c4bf722e12491081914eef98c51ca8d8e13a7f1f
Reid Wahl <nrwahl@protonmail.com>
2023-12-28 16:28:18 -0800

    API: libcrmcommon: Deprecate XML_RSC_ATTR_INTERLEAVE
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml_compat.h
+++ include/crm/msg_xml_compat.h
@@ -178,6 +178,9 @@ extern "C" {
 //! \deprecated Do not use
 #define XML_RSC_ATTR_ORDERED "ordered"
 
+//! \deprecated Do not use
+#define XML_RSC_ATTR_INTERLEAVE "interleave"
+
 #ifdef __cplusplus
 }
 #endif
