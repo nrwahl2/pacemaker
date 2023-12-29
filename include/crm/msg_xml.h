095ae9e90738c45129f13b7ce1c632ab8dec5795
Reid Wahl <nrwahl@protonmail.com>
2023-12-28 17:28:18 -0800

    API: libcrmcommon: Deprecate PCMK_META_CLONE_MAX
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/msg_xml.h
+++ include/crm/msg_xml.h
@@ -61,7 +61,6 @@ extern "C" {
  * Meta attributes
  */
 
-#define PCMK_META_CLONE_MAX                 "clone-max"
 #define PCMK_META_CLONE_MIN                 "clone-min"
 #define PCMK_META_CLONE_NODE_MAX            "clone-node-max"
 #define PCMK_META_ENABLED                   "enabled"
