f149ee3c36061bd1b55883474ffa1b548332c6eb
Reid Wahl <nrwahl@protonmail.com>
2023-12-28 21:14:26 -0800

    Refactor: libcrmcommon: New PCMK__META_REMOTE_NODE string constant
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- include/crm/common/options_internal.h
+++ include/crm/common/options_internal.h
@@ -300,6 +300,7 @@ bool pcmk__valid_sbd_timeout(const char *value);
 #define PCMK__META_PROMOTABLE               "promotable"
 #define PCMK__META_PROMOTED_MAX             "promoted-max"
 #define PCMK__META_PROMOTED_NODE_MAX        "promoted-node-max"
+#define PCMK__META_REMOTE_NODE              "remote-node"
 #define PCMK__META_REQUIRES                 "requires"
 #define PCMK__META_RESTART_TYPE             "restart-type"
 #define PCMK__META_RESOURCE_STICKINESS      "resource-stickiness"
