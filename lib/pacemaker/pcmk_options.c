acb70d9fb6b2841c7ea0c7ed33d58a98020e0159
Reid Wahl <nrwahl@protonmail.com>
2024-02-23 00:37:08 -0800

    Refactor: libpacemaker: New pcmk__list_fence_attrs()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/pacemaker/pcmk_options.c
+++ lib/pacemaker/pcmk_options.c
@@ -62,3 +62,30 @@ pcmk_list_cluster_options(xmlNode **xml, bool all)
     pcmk__xml_output_finish(out, pcmk_rc2exitc(rc), xml);
     return rc;
 }
+
+/*!
+ * \internal
+ * \brief List special fence device instance attributes
+ *
+ * These are attributes that can apply to any fence device.
+ *
+ * \param[in,out] out  Output object
+ * \param[in]     all  If \c true, include advanced and deprecated options (this
+ *                     is always treated as true for XML output objects)
+ *
+ * \return Standard Pacemaker return code
+ */
+int
+pcmk__list_fence_attrs(pcmk__output_t *out, bool all)
+{
+    const char *name = "fence-attributes";
+    const char *desc_short = "Fence device instance attributes";
+    const char *desc_long = NULL;
+
+    desc_long = "Special instance attributes for fence devices. These can be "
+                "used with any fence device, regardless of type. They're "
+                "handled by Pacemaker, not by the fence agent or the fencing "
+                "library.";
+
+    return pcmk__output_fence_attrs(out, name, desc_short, desc_long, all);
+}
