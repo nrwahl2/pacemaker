nrwahl2/pacemaker

commit c6d9cea906065522e41ab53974cc36ecc7682f1d

Refactor: libcrmcommon: New pcmk__xml_escape()
Replaces xmlEncodeEntitiesReentrant() and crm_xml_escape()

Fixes T768

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/xml.c
@@ -1333,6 +1333,108 @@ replace_text(char *text, int index, size_t *length, const char *replace)
 }
 
 /*!
+ * \internal
+ * \brief Replace special characters with their XML escape sequences
+ *
+ * XML allows the escaping of special characters by replacing them with entity
+ * references (for example, <tt>"&quot;"</tt>) or character references (for
+ * example, <tt>"&#13;"</tt>).
+ *
+ * The special characters <tt>'<'</tt> and <tt>'&'</tt> are not allowed in their
+ * literal forms in XML character data. Character data is non-markup text (for
+ * example, the content of a text node).
+ *
+ * Additionally, if an attribute value is delimited by single quotes, then
+ * single quotes must be escaped within the value. Similarly, if an attribute
+ * value is delimited by double quotes, then double quotes must be escaped
+ * within the value.
+ *
+ * For more details, see the "Character Data and Markup" section of the XML
+ * spec, currently section 2.4:
+ * https://www.w3.org/TR/xml/#dt-markup
+ *
+ * Pacemaker always delimits attribute values with double quotes, so this
+ * function doesn't escape single quotes.
+ *
+ * \param[in] text          Text to escape
+ * \param[in] escape_quote  If \c true, escape double quotes (should be enabled
+ *                          for attribute values)
+ *
+ * \return Newly allocated string equivalent to \p text but with special
+ *         characters replaced with XML escape sequences (or \c NULL if \p text
+ *         is \c NULL). If \p text is not \c NULL, the return value is
+ *         guaranteed not to be \c NULL.
+ *
+ * \note There are libxml functions that purport to do this:
+ *       \c xmlEncodeEntitiesReentrant() and \c xmlEncodeSpecialChars().
+ *       However, their escaping is incomplete. See:
+ *       https://discourse.gnome.org/t/intended-use-of-xmlencodeentitiesreentrant-vs-xmlencodespecialchars/19252
+ */
+char *
+pcmk__xml_escape(const char *text, bool escape_quote)
+{
+    size_t length = 0;
+    char *copy = NULL;
+    char buf[32] = { '\0', };
+
+    if (text == NULL) {
+        return NULL;
+    }
+    length = strlen(text);
+    pcmk__str_update(&copy, text);
+
+    for (size_t index = 0; index < length; index++) {
+        if (((copy[index] & 0x80) != 0) && ((copy[index + 1] & 0x80) != 0)) {
+            /* @TODO Is this a valid test for multi-byte Unicode characters?
+             * Probably better to escape them anyway.
+             */
+            index++;
+            continue;
+        }
+
+        switch (copy[index]) {
+            case 0:
+                // Sanity only; loop should stop at the last non-null byte
+                break;
+            case '<':
+                copy = replace_text(copy, index, &length, "&lt;");
+                break;
+            case '>':
+                // Not necessary, but for symmetry with '<'
+                copy = replace_text(copy, index, &length, "&gt;");
+                break;
+            case '&':
+                copy = replace_text(copy, index, &length, "&amp;");
+                break;
+            case '"':
+                if (escape_quote) {
+                    copy = replace_text(copy, index, &length, "&quot;");
+                }
+                break;
+            case '\n':
+            case '\t':
+                // Don't escape newlines and tabs
+                break;
+            default:
+                if ((copy[index] < 0x20) || (copy[index] >= 0x7f)) {
+                    /* Escape non-printing and Unicode characters (0x7f (delete)
+                     * is ASCII but non-printing)
+                     *
+                     * @TODO Handle multi-byte Unicode characters
+                     */
+                    snprintf(buf, sizeof(buf), "&#%.2x;", copy[index]);
+                    copy = replace_text(copy, index, &length, buf);
+
+                    // Jump to semicolon; loop counter will skip one more char
+                    index += strlen(buf) - 1;
+                }
+                break;
+        }
+    }
+    return copy;
+}
+
+/*!
  * \brief Replace special characters with their XML escape sequences
  *
  * \param[in] text  Text to escape

include/crm/common/xml_internal.h
@@ -217,6 +217,8 @@ void pcmk__xe_remove_matching_attrs(xmlNode *element,
 
 GString *pcmk__element_xpath(const xmlNode *xml);
 
+char *pcmk__xml_escape(const char *text, bool escape_quote);
+
 /*!
  * \internal
  * \brief Get the root directory to scan XML artefacts of given kind for

lib/common/options.c
@@ -957,7 +957,7 @@ static void
 add_desc(GString *s, const char *tag, const char *desc, const char *values,
          const char *spaces)
 {
-    char *escaped_en = crm_xml_escape(desc);
+    char *escaped_en = pcmk__xml_escape(desc, false);
 
     if (spaces != NULL) {
         g_string_append(s, spaces);
@@ -983,7 +983,7 @@ add_desc(GString *s, const char *tag, const char *desc, const char *values,
     {
         static const char *locale = NULL;
 
-        char *localized = crm_xml_escape(_(desc));
+        char *localized = pcmk__xml_escape(_(desc), false);
 
         if (strcmp(escaped_en, localized) != 0) {
             if (locale == NULL) {

lib/common/xml_attr.c
@@ -75,7 +75,7 @@ pcmk__dump_xml_attr(const xmlAttr *attr, GString *buffer)
     }
 
     p_name = (const char *) attr->name;
-    p_value = crm_xml_escape((const char *)attr->children->content);
+    p_value = pcmk__xml_escape((const char *) attr->children->content, true);
     pcmk__g_strcat(buffer, " ", p_name, "=\"", pcmk__s(p_value, "<null>"), "\"",
                    NULL);
 

lib/common/xml_display.c
@@ -128,7 +128,7 @@ show_xml_element(pcmk__output_t *out, GString *buffer, const char *prefix,
                 pcmk__str_update(&p_copy, "*****");
 
             } else {
-                p_copy = crm_xml_escape(p_value);
+                p_copy = pcmk__xml_escape(p_value, true);
             }
 
             pcmk__g_strcat(buffer, " ", p_name, "=\"",

lib/fencing/st_lha.c
@@ -203,8 +203,8 @@ stonith__lha_metadata(const char *agent, int timeout, char **output)
     }
 
     if (lha_agents_lib && st_new_fn && st_del_fn && st_info_fn && st_log_fn) {
-        char *xml_meta_longdesc = NULL;
-        char *xml_meta_shortdesc = NULL;
+        char *meta_longdesc_escaped = NULL;
+        char *meta_shortdesc_escaped = NULL;
 
         char *meta_param = NULL;
         char *meta_longdesc = NULL;
@@ -241,25 +241,22 @@ stonith__lha_metadata(const char *agent, int timeout, char **output)
             return -EINVAL;
         }
 
-        xml_meta_longdesc =
-            (char *)xmlEncodeEntitiesReentrant(NULL, (const unsigned char *)meta_longdesc);
-        xml_meta_shortdesc =
-            (char *)xmlEncodeEntitiesReentrant(NULL, (const unsigned char *)meta_shortdesc);
+        meta_longdesc_escaped = pcmk__xml_escape(meta_longdesc, false);
+        meta_shortdesc_escaped = pcmk__xml_escape(meta_shortdesc, false);
 
         /* @TODO This needs a string that's parsable by crm_get_msec(). In
          * general, pcmk__readable_interval() doesn't provide that. It works
          * here because PCMK_DEFAULT_ACTION_TIMEOUT_MS is 20000 -> "20s".
          */
         timeout_str = pcmk__readable_interval(PCMK_DEFAULT_ACTION_TIMEOUT_MS);
-        buffer = crm_strdup_printf(META_TEMPLATE, agent, xml_meta_longdesc,
-                                   xml_meta_shortdesc, meta_param,
+        buffer = crm_strdup_printf(META_TEMPLATE, agent, meta_longdesc_escaped,
+                                   meta_shortdesc_escaped, meta_param,
                                    timeout_str, timeout_str, timeout_str);
 
-        xmlFree(xml_meta_longdesc);
-        xmlFree(xml_meta_shortdesc);
-
-        free(meta_shortdesc);
+        free(meta_longdesc_escaped);
+        free(meta_shortdesc_escaped);
         free(meta_longdesc);
+        free(meta_shortdesc);
         free(meta_param);
     }
     if (output) {

lib/pengine/pe_output.c
@@ -1545,14 +1545,13 @@ failed_action_xml(pcmk__output_t *out, va_list args) {
     const char *call_id = crm_element_value(xml_op, PCMK__XA_CALL_ID);
     const char *exitstatus = NULL;
     const char *exit_reason = crm_element_value(xml_op, PCMK_XA_EXIT_REASON);
+    char *exit_reason_esc = pcmk__xml_escape(pcmk__s(exit_reason, "none"),
+                                             true);
     const char *status_s = NULL;
 
     time_t epoch = 0;
     char *rc_s = NULL;
-    char *reason_s = crm_xml_escape(exit_reason ? exit_reason : "none");
     xmlNodePtr node = NULL;
-
-    exit_reason = pcmk__s(reason_s, "");
 
     pcmk__scan_min_int(crm_element_value(xml_op, PCMK__XA_RC_CODE), &rc, 0);
     pcmk__scan_min_int(crm_element_value(xml_op, PCMK__XA_OP_STATUS), &status,
@@ -1568,7 +1567,7 @@ failed_action_xml(pcmk__output_t *out, va_list args) {
                                         op_key_name, op_key,
                                         PCMK_XA_NODE, uname,
                                         PCMK_XA_EXITSTATUS, exitstatus,
-                                        PCMK_XA_EXITREASON, exit_reason,
+                                        PCMK_XA_EXITREASON, exit_reason_esc,
                                         PCMK_XA_EXITCODE, rc_s,
                                         PCMK_XA_CALL, call_id,
                                         PCMK_XA_STATUS, status_s,
@@ -1603,7 +1602,7 @@ failed_action_xml(pcmk__output_t *out, va_list args) {
         free(rc_change);
     }
 
-    free(reason_s);
+    free(exit_reason_esc);
     return pcmk_rc_ok;
 }
 

lib/services/services_lsb.c
@@ -80,14 +80,6 @@
 #define SHORT_DESC                      "# Short-Description:"
 #define DESCRIPTION                     "# Description:"
 
-#define lsb_meta_helper_free_value(m)           \
-    do {                                        \
-        if ((m) != NULL) {                      \
-            xmlFree(m);                         \
-            (m) = NULL;                         \
-        }                                       \
-    } while(0)
-
 /*!
  * \internal
  * \brief Grab an LSB header value
@@ -102,7 +94,7 @@ static inline gboolean
 lsb_meta_helper_get_value(const char *line, char **value, const char *prefix)
 {
     if (!*value && pcmk__starts_with(line, prefix)) {
-        *value = (char *)xmlEncodeEntitiesReentrant(NULL, BAD_CAST line+strlen(prefix));
+        *value = pcmk__xml_escape(line + strlen(prefix), false);
         return TRUE;
     }
     return FALSE;
@@ -205,9 +197,7 @@ services__get_lsb_metadata(const char *type, char **output)
             }
 
             // Make long description safe to use in XML
-            long_desc =
-                (char *) xmlEncodeEntitiesReentrant(NULL,
-                                                    (pcmkXmlStr) desc->str);
+            long_desc = pcmk__xml_escape(desc->str, false);
             g_string_free(desc, TRUE);
 
             if (processed_line) {
@@ -237,15 +227,15 @@ services__get_lsb_metadata(const char *type, char **output)
                                 pcmk__s(default_start, ""),
                                 pcmk__s(default_stop, ""));
 
-    lsb_meta_helper_free_value(long_desc);
-    lsb_meta_helper_free_value(short_desc);
-    lsb_meta_helper_free_value(provides);
-    lsb_meta_helper_free_value(required_start);
-    lsb_meta_helper_free_value(required_stop);
-    lsb_meta_helper_free_value(should_start);
-    lsb_meta_helper_free_value(should_stop);
-    lsb_meta_helper_free_value(default_start);
-    lsb_meta_helper_free_value(default_stop);
+    free(long_desc);
+    free(short_desc);
+    free(provides);
+    free(required_start);
+    free(required_stop);
+    free(should_start);
+    free(should_stop);
+    free(default_start);
+    free(default_stop);
     return pcmk_ok;
 }
 

commit 7375093a59513923184768cb1147b7c4cb415720

Fix: libcrmcommon: Escape newlines and tabs in XML attribute values
Regression introduced by c6d9cea9, sort of.

Before that commit, newlines and tabs were escaped as "\\n" and "\\t",
respectively. These are not valid XML escape sequences.

We made the decision not to escape newlines and tabs when we added the
pcmk__xml_escape() function, with the caveat that the "right" answer was
unclear:

"If not, the XML will be more visually understandable, but conformant
XML parsers will treat the newline as a space (for example, if something
extracts the text and displays it to the user). If we do, the parser
case may be closer to our original intent, but the XML is harder to
read, and it may not be appropriate (for example, the parsing tool might
prefer to do line wrapping of a textual paragraph on its own)."
https://github.com/ClusterLabs/pacemaker/pull/3323#discussion_r1474790296

However, now if an attribute value contains a newline or tab, it gets
replaced by a space character. Likewise, a sequence of newlines or tabs
would get replaced by a single space character.

In rare use cases, an attribute value may contain newlines or tabs that
need to be preserved. So here, we replace them with their respective
character references.

Also add separate unit test cases for newline and tab, since they're now
handled differently depending on whether for_attr is true. Separate out
the carriage return test from the non-printing characters test, since it
does print (in a sense) and we want to make it explicit that it's
handled differently from the other whitespace characters.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

lib/common/xml.c
@@ -1024,14 +1024,14 @@ replace_text(char *text, size_t *index, size_t *length, const char *replace)
  *
  * See \c pcmk__xml_escape() for more details.
  *
- * \param[in] text          String to check
- * \param[in] escape_quote  If \c true, double quotes must be escaped
+ * \param[in] text      String to check
+ * \param[in] for_attr  If \c true, escape extra characters for attribute value
  *
  * \return \c true if \p text has special characters that need to be escaped, or
  *         \c false otherwise
  */
 bool
-pcmk__xml_needs_escape(const char *text, bool escape_quote)
+pcmk__xml_needs_escape(const char *text, bool for_attr)
 {
     size_t length = 0;
 
@@ -1051,18 +1051,20 @@ pcmk__xml_needs_escape(const char *text, bool escape_quote)
             case '<':
                 return true;
             case '>':
-                // Not necessary, but for symmetry with '<'
                 return true;
             case '&':
+                // @TODO Don't escape if it's the beginning of an entity ref
                 return true;
             case '"':
-                if (escape_quote) {
+                if (for_attr) {
                     return true;
                 }
                 break;
             case '\n':
             case '\t':
-                // Don't escape newline or tab
+                if (for_attr) {
+                    return true;
+                }
                 break;
             default:
                 if ((text[index] < 0x20) || (text[index] >= 0x7F)) {
@@ -1083,25 +1085,45 @@ pcmk__xml_needs_escape(const char *text, bool escape_quote)
  * references (for example, <tt>"&quot;"</tt>) or character references (for
  * example, <tt>"&#13;"</tt>).
  *
- * The special characters <tt>'<'</tt> and <tt>'&'</tt> are not allowed in their
- * literal forms in XML character data. Character data is non-markup text (for
- * example, the content of a text node).
- *
- * Additionally, if an attribute value is delimited by single quotes, then
- * single quotes must be escaped within the value. Similarly, if an attribute
- * value is delimited by double quotes, then double quotes must be escaped
- * within the value.
+ * The special characters <tt>'&'</tt> (except as the beginning of an entity
+ * reference) and <tt>'<'</tt> are not allowed in their literal forms in XML
+ * character data. Character data is non-markup text (for example, the content
+ * of a text node). <tt>'>'</tt> is allowed under most circumstances; we escape
+ * it for safety and symmetry.
  *
  * For more details, see the "Character Data and Markup" section of the XML
  * spec, currently section 2.4:
  * https://www.w3.org/TR/xml/#dt-markup
  *
+ * Attribute values are handled specially.
+ * * If an attribute value is delimited by single quotes, then single quotes
+ *   must be escaped within the value.
+ * * Similarly, if an attribute value is delimited by double quotes, then double
+ *   quotes must be escaped within the value.
+ * * A conformant XML processor replaces a literal whitespace character (tab,
+ *   newline, carriage return, space) in an attribute value with a space
+ *   (\c '#x20') character. However, a reference to a whitespace character (for
+ *   example, \c "&#x0A;" for \c '\n') does not get replaced. The processor then
+ *   replaces any sequence of space (\c '#x20') characters with a single space
+ *   character.
+ *   * For more details, see the "Attribute-Value Normalization" section of the
+ *     XML spec, currently section 3.3.3:
+ *     https://www.w3.org/TR/xml/#AVNormalize
+ *
  * Pacemaker always delimits attribute values with double quotes, so this
  * function doesn't escape single quotes.
  *
- * \param[in] text          Text to escape
- * \param[in] escape_quote  If \c true, escape double quotes (should be enabled
- *                          for attribute values)
+ * We sometimes need to preserve literal whitespace characters in attribute
+ * values, so this function replaces them by their respective character
+ * references. However, a processor will still replace a sequence of space
+ * (\c '#x20') characters with a single one, unless the attribute type is CDATA;
+ * using character references (\c "&#x20;") cannot prevent that.
+ *
+ * This function always escapes carriage returns even outside of attribute
+ * values, since displaying them as literals would be messy.
+ *
+ * \param[in] text      Text to escape
+ * \param[in] for_attr  If \c true, escape extra characters for attribute value
  *
  * \return Newly allocated string equivalent to \p text but with special
  *         characters replaced with XML escape sequences (or \c NULL if \p text
@@ -1114,7 +1136,7 @@ pcmk__xml_needs_escape(const char *text, bool escape_quote)
  *       https://discourse.gnome.org/t/intended-use-of-xmlencodeentitiesreentrant-vs-xmlencodespecialchars/19252
  */
 char *
-pcmk__xml_escape(const char *text, bool escape_quote)
+pcmk__xml_escape(const char *text, bool for_attr)
 {
     size_t length = 0;
     char *copy = NULL;
@@ -1138,20 +1160,26 @@ pcmk__xml_escape(const char *text, bool escape_quote)
                 copy = replace_text(copy, &index, &length, "&lt;");
                 break;
             case '>':
-                // Not necessary, but for symmetry with '<'
                 copy = replace_text(copy, &index, &length, "&gt;");
                 break;
             case '&':
+                // @TODO Don't escape if it's the beginning of an entity ref
                 copy = replace_text(copy, &index, &length, "&amp;");
                 break;
             case '"':
-                if (escape_quote) {
+                if (for_attr) {
                     copy = replace_text(copy, &index, &length, "&quot;");
                 }
                 break;
             case '\n':
+                if (for_attr) {
+                    copy = replace_text(copy, &index, &length, "&#x0A;");
+                }
+                break;
             case '\t':
-                // Don't escape newlines and tabs
+                if (for_attr) {
+                    copy = replace_text(copy, &index, &length, "&#x09;");
+                }
                 break;
             default:
                 if ((copy[index] < 0x20) || (copy[index] >= 0x7F)) {

lib/common/tests/xml/pcmk__xml_escape_test.c
@@ -39,7 +39,7 @@ escape_unchanged(void **state)
     const char *unchanged = "abcdefghijklmnopqrstuvwxyz"
                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                             "0123456789"
-                            "\n\t`~!@#$%^*()-_=+/|\\[]{}?.,'";
+                            "`~!@#$%^*()-_=+/|\\[]{}?.,'";
     char *str = NULL;
 
     str = pcmk__xml_escape(unchanged, false);
@@ -118,10 +118,58 @@ escape_double_quote(void **state)
 }
 
 static void
+escape_newline(void **state)
+{
+    const char *newline = "\nabc\ndef\n";
+    const char *newline_esc = "&#x0A;abc&#x0A;def&#x0A;";
+    char *str = NULL;
+
+    str = pcmk__xml_escape(newline, false);
+    assert_string_equal(str, newline);
+    free(str);
+
+    str = pcmk__xml_escape(newline, true);
+    assert_string_equal(str, newline_esc);
+    free(str);
+}
+
+static void
+escape_tab(void **state)
+{
+    const char *tab = "\tabc\tdef\t";
+    const char *tab_esc = "&#x09;abc&#x09;def&#x09;";
+    char *str = NULL;
+
+    str = pcmk__xml_escape(tab, false);
+    assert_string_equal(str, tab);
+    free(str);
+
+    str = pcmk__xml_escape(tab, true);
+    assert_string_equal(str, tab_esc);
+    free(str);
+}
+
+static void
+escape_carriage_return(void **state)
+{
+    const char *cr = "\rabc\rdef\r";
+    const char *cr_esc = "&#x0D;abc&#x0D;def&#x0D;";
+    char *str = NULL;
+
+    str = pcmk__xml_escape(cr, false);
+    assert_string_equal(str, cr_esc);
+    free(str);
+
+    str = pcmk__xml_escape(cr, true);
+    assert_string_equal(str, cr_esc);
+    free(str);
+}
+
+static void
 escape_nonprinting(void **state)
 {
-    const char *nonprinting = "\a\r\x7F\x1B";
-    const char *nonprinting_esc = "&#x07;&#x0D;&#x7F;&#x1B;";
+    const char *nonprinting = "\a\x7F\x1B";
+    const char *nonprinting_esc = "&#x07;&#x7F;&#x1B;";
     char *str = NULL;
 
     str = pcmk__xml_escape(nonprinting, false);
@@ -188,5 +236,8 @@ PCMK__UNIT_TEST(NULL, NULL,
                 cmocka_unit_test(escape_right_angle),
                 cmocka_unit_test(escape_ampersand),
                 cmocka_unit_test(escape_double_quote),
+                cmocka_unit_test(escape_newline),
+                cmocka_unit_test(escape_tab),
+                cmocka_unit_test(escape_carriage_return),
                 cmocka_unit_test(escape_nonprinting),
                 cmocka_unit_test(escape_utf8));

lib/common/tests/xml/pcmk__xml_needs_escape_test.c
@@ -29,7 +29,7 @@ escape_unchanged(void **state)
     const char *unchanged = "abcdefghijklmnopqrstuvwxyz"
                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                             "0123456789"
-                            "\n\t`~!@#$%^*()-_=+/|\\[]{}?.,'";
+                            "`~!@#$%^*()-_=+/|\\[]{}?.,'";
 
     assert_false(pcmk__xml_needs_escape(unchanged, false));
     assert_false(pcmk__xml_needs_escape(unchanged, true));
@@ -102,6 +102,54 @@ escape_double_quote(void **state)
 }
 
 static void
+escape_newline(void **state)
+{
+    const char *newline_left = "\nabcdef";
+    const char *newline_mid = "abc\ndef";
+    const char *newline_right = "abcdef\n";
+
+    assert_false(pcmk__xml_needs_escape(newline_left, false));
+    assert_false(pcmk__xml_needs_escape(newline_mid, false));
+    assert_false(pcmk__xml_needs_escape(newline_right, false));
+
+    assert_true(pcmk__xml_needs_escape(newline_left, true));
+    assert_true(pcmk__xml_needs_escape(newline_mid, true));
+    assert_true(pcmk__xml_needs_escape(newline_right, true));
+}
+
+static void
+escape_tab(void **state)
+{
+    const char *tab_left = "\tabcdef";
+    const char *tab_mid = "abc\tdef";
+    const char *tab_right = "abcdef\t";
+
+    assert_false(pcmk__xml_needs_escape(tab_left, false));
+    assert_false(pcmk__xml_needs_escape(tab_mid, false));
+    assert_false(pcmk__xml_needs_escape(tab_right, false));
+
+    assert_true(pcmk__xml_needs_escape(tab_left, true));
+    assert_true(pcmk__xml_needs_escape(tab_mid, true));
+    assert_true(pcmk__xml_needs_escape(tab_right, true));
+}
+
+static void
+escape_carriage_return(void **state)
+{
+    const char *cr_left = "\rabcdef";
+    const char *cr_mid = "abc\rdef";
+    const char *cr_right = "abcdef\r";
+
+    assert_true(pcmk__xml_needs_escape(cr_left, false));
+    assert_true(pcmk__xml_needs_escape(cr_mid, false));
+    assert_true(pcmk__xml_needs_escape(cr_right, false));
+
+    assert_true(pcmk__xml_needs_escape(cr_left, true));
+    assert_true(pcmk__xml_needs_escape(cr_mid, true));
+    assert_true(pcmk__xml_needs_escape(cr_right, true));
+}
+
+static void
 escape_nonprinting(void **state)
 {
     const char *alert_left = "\aabcdef";
@@ -112,7 +160,7 @@ escape_nonprinting(void **state)
     const char *delete_mid = "abc\x7F""def";
     const char *delete_right = "abcdef\x7F";
 
-    const char *nonprinting_all = "\a\r\x7F\x1B";
+    const char *nonprinting_all = "\a\x7F\x1B";
 
     assert_true(pcmk__xml_needs_escape(alert_left, false));
     assert_true(pcmk__xml_needs_escape(alert_mid, false));
@@ -174,5 +222,8 @@ PCMK__UNIT_TEST(NULL, NULL,
                 cmocka_unit_test(escape_right_angle),
                 cmocka_unit_test(escape_ampersand),
                 cmocka_unit_test(escape_double_quote),
+                cmocka_unit_test(escape_newline),
+                cmocka_unit_test(escape_tab),
+                cmocka_unit_test(escape_carriage_return),
                 cmocka_unit_test(escape_nonprinting),
                 cmocka_unit_test(escape_utf8));