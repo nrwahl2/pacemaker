4395989c93ddbea6e33cff67d8e492a3ddfc1bf0
Reid Wahl <nrwahl@protonmail.com>
2024-04-01 02:19:03 -0700

    API: libcrmcommon: Deprecate crm_xml_sanitize_id()
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/xml.c
+++ lib/common/xml.c
@@ -1175,26 +1175,6 @@ pcmk__xe_add_last_written(xmlNode *xe)
 }
 
 /*!
- * \brief Sanitize a string so it is usable as an XML ID
- *
- * \param[in,out] id  String to sanitize
- */
-void
-crm_xml_sanitize_id(char *id)
-{
-    char *c;
-
-    for (c = id; *c; ++c) {
-        /* @TODO Sanitize more comprehensively */
-        switch (*c) {
-            case ':':
-            case '#':
-                *c = '.';
-        }
-    }
-}
-
-/*!
  * \internal
  * \brief Check whether a string has XML special characters that must be escaped
  *
@@ -2960,6 +2940,21 @@ xmlNode *
 expand_idref(xmlNode *input, xmlNode *top)
 {
     return pcmk__xe_resolve_idref(input, top);
+}
+
+void
+crm_xml_sanitize_id(char *id)
+{
+    char *c;
+
+    for (c = id; *c; ++c) {
+        /* @TODO Sanitize more comprehensively */
+        switch (*c) {
+            case ':':
+            case '#':
+                *c = '.';
+        }
+    }
 }
 
 void
