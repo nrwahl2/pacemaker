b406de3af3ab2954af7b7764b9af2c8630d58b71
Reid Wahl <nrwahl@protonmail.com>
2026-08-09 14:15:59 -0700

    Refactor: cts: Drop killal() terminate argument
    All callers pass the default.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- python/pacemaker/_cts/process.py
+++ python/pacemaker/_cts/process.py
@@ -12,7 +12,7 @@ import psutil
 from pacemaker.exitstatus import ExitStatus
 
 
-def killall(process_names, terminate=False):
+def killall(process_names):
     """Kill all instances of every process in a list."""
     if not process_names:
         return
@@ -20,19 +20,9 @@ def killall(process_names, terminate=False):
     if not isinstance(process_names, list):
         process_names = [process_names]
 
-    procs = []
     for proc in psutil.process_iter(["name"]):
         if proc.info["name"] in process_names:
-            procs.append(proc)
-
-    if terminate:
-        for proc in procs:
-            proc.terminate()
-        _, alive = psutil.wait_procs(procs, timeout=3)
-        procs = alive
-
-    for proc in procs:
-        proc.kill()
+            proc.kill()
 
 
 def is_proc_running(process_name):
