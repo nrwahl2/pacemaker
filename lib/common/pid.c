99c146fdee8e586b887c06f38270c189f07c44b3
Reid Wahl <nrwahl@protonmail.com>
2026-08-23 20:10:56 -0700

    Refactor: libcrmcommon: Unindent outer block of pcmk__pid_active()
    And clean up a few comments.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- lib/common/pid.c
+++ lib/common/pid.c
@@ -22,17 +22,21 @@ pcmk__pid_active(pid_t pid, const char *expected_path)
     int kill_rc = 0;
     int rc = pcmk_rc_ok;
 
+    bool paths_equal = false;
+    char *found_path = NULL;
+
     if (pid <= 0) {
         return EINVAL;
     }
 
     kill_rc = kill(pid, 0);
+
     if ((kill_rc < 0) && (errno == ESRCH)) {
-        return ESRCH;  /* no such PID detected */
+        return ESRCH;   // No such PID detected
+    }
 
-    } else if ((expected_path == NULL) || !pcmk__procfs_has_pids()) {
-        // The kill result is all we have, we can't check the name
-
+    if ((expected_path == NULL) || !pcmk__procfs_has_pids()) {
+        // The kill result is all we have; we can't check the name
         if (kill_rc == 0) {
             return pcmk_rc_ok;
         }
@@ -45,48 +49,46 @@ pcmk__pid_active(pid_t pid, const char *expected_path)
             last_asked_pid = pid;
         }
 
-        return rc; /* errno != ESRCH */
-
-    } else {
-        /* make sure PID hasn't been reused by another process
-           XXX: might still be just a zombie, which could confuse decisions */
-        bool paths_equal = false;
-        char *found_path = NULL;
-
-        rc = pcmk__procfs_pid2path(pid, &found_path);
-        paths_equal = pcmk__str_eq(found_path, expected_path, pcmk__str_none);
-        free(found_path);
-
-        if (rc == pcmk_rc_ok) {
-            return paths_equal? pcmk_rc_ok : ESRCH;
-        }
-
-        // On non-EACCES, check again to filter out races
-        if ((rc != EACCES) && (kill(pid, 0) < 0) && (errno == ESRCH)) {
-            return ESRCH;
-        }
-
-        if (last_asked_pid != pid) {
-            if (rc == EACCES) {
-                pcmk__info("Could not get executable for PID %lld: %s "
-                           QB_XS " rc=%d", (long long) pid, pcmk_rc_str(rc),
-                           rc);
-
-            } else {
-                pcmk__err("Could not get executable for PID %lld: %s "
-                          QB_XS " rc=%d",(long long) pid, pcmk_rc_str(rc), rc);
-            }
-
-            last_asked_pid = pid;
-        }
-
-        if (rc == EACCES) {
-            // Trust kill if it was OK (we can't double-check via path)
-            return (kill_rc == 0)? pcmk_rc_ok : EACCES;
-        }
-
-        // Most likely errno == ENOENT
+        // rc != ESRCH
+        return rc;
     }
 
+    /* Make sure the PID hasn't been reused by another process.
+     *
+     * @FIXME The PID might still belong to a zombie, which could confuse
+     * decisions.
+     */
+    rc = pcmk__procfs_pid2path(pid, &found_path);
+    paths_equal = pcmk__str_eq(found_path, expected_path, pcmk__str_none);
+    free(found_path);
+
+    if (rc == pcmk_rc_ok) {
+        return paths_equal? pcmk_rc_ok : ESRCH;
+    }
+
+    // On non-EACCES, check again to filter out races
+    if ((rc != EACCES) && (kill(pid, 0) < 0) && (errno == ESRCH)) {
+        return ESRCH;
+    }
+
+    if (last_asked_pid != pid) {
+        if (rc == EACCES) {
+            pcmk__info("Could not get executable for PID %lld: %s "
+                       QB_XS " rc=%d", (long long) pid, pcmk_rc_str(rc), rc);
+
+        } else {
+            pcmk__err("Could not get executable for PID %lld: %s "
+                      QB_XS " rc=%d",(long long) pid, pcmk_rc_str(rc), rc);
+        }
+
+        last_asked_pid = pid;
+    }
+
+    if (rc == EACCES) {
+        // Trust kill if it was OK (we can't double-check via path)
+        return (kill_rc == 0)? pcmk_rc_ok : EACCES;
+    }
+
+    // Most likely rc == ENOENT
     return ESRCH;
 }
